6a697b3efa2b9c5c2bb9cc5d9308d1542935d0cc
Sushant Pandey <sushant.pandey@247software.com>
2025-12-10 17:17:54 +0530

    Update MongoDB connection URI to placeholder
    Replaced sensitive MongoDB URI with a placeholder.

--- watch_changes.py
+++ watch_changes.py
@@ -3,7 +3,7 @@ from pymongo.errors import PyMongoError
 import threading
 
 # MongoDB connection
-mongoUri= "mongodb+srv://myAtlasDBUser:Sushant.1729@myatlasclusteredu.e4bfj5j.mongodb.net/"
+mongoUri= "URI STRING"
 client = MongoClient(mongoUri)
 data=input("Enter database name: ")
 db = client[data]
